b83c701c54faa5accf2a733e99d6dd8df7028741
CarolinaChamorro <carolinalisicsb@gmail.com>
2021-02-17 09:42:27 -0500

    ejercicios grupal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ContadorComponent } from './Ejercicio1/contador.component';
+import { EstudiantesComponent } from './Ejercicio2/estudiantes.component';
+import { ListadoComponent } from './Ejercicio3/listado/listado.component';
+import { EjemploIfComponent } from './EjercicioGrupal/ejemplo-if/ejemplo-if.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContadorComponent,
+    EstudiantesComponent,
+    ListadoComponent,
+    EjemploIfComponent
   ],
   imports: [
     BrowserModule
